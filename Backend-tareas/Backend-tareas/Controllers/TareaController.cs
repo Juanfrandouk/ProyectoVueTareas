98a8eba8b6a5719d607ac4b6a87621f31a02b4ba
Juanfrandouk <61378658+Juanfrandouk@users.noreply.github.com>
2023-04-24 18:17:32 +0200

    first commit

---
+++ Backend-tareas/Backend-tareas/Controllers/TareaController.cs
@@ -0,0 +1,95 @@
+﻿using Backend_tareas.Context;
+using Backend_tareas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_tareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+
+        public TareaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listTareas = await _context.Tareas.ToListAsync();
+                return Ok(listTareas);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Tarea tarea)
+        {
+            try
+            {
+                _context.Tareas.Add(tarea);
+                await _context.SaveChangesAsync();
+                return Ok(new {messsage ="La tarea fue registrada con exito" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Tarea tarea)
+        {
+            try
+            {
+                if (id != tarea.Id)
+                {   
+                    return NotFound("id incorrecto");
+                }                           
+              tarea.estado = !tarea.estado;
+                _context.Entry(tarea).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(new { messsage = "La tarea fue actualizada con exito" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+
+                var tarea = await _context.Tareas.FindAsync(id);
+                if (tarea == null)
+                {
+                    return NotFound("id incorrecto");
+                }
+                 _context.Tareas.Remove(tarea);
+                await _context.SaveChangesAsync();
+                return Ok(new { messsage = "La tarea fue eliminada con exito" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Backend-tareas/Backend-tareas/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/Backend_tareas.Context.AplicationDbContext.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220920092242_v1.0')
+BEGIN
+    CREATE TABLE [Tareas] (
+        [Id] int NOT NULL IDENTITY,
+        [Nombre] varchar(100) NOT NULL,
+        [estado] bit NOT NULL,
+        CONSTRAINT [PK_Tareas] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220920092242_v1.0')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220920092242_v1.0', N'3.1.29');
+END;
+
+GO
+
